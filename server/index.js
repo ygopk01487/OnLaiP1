cc3d3342d8054651957992cf7bced84aefb19a4b
ygopk01487 <misublack24@gmail.com>
2024-05-10 13:10:58 +0700

    updates delete file vercel json

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://storebook-client.vercel.app",
+    origin: "https://localhost:5173",
   },
 });
 
