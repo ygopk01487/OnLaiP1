3883063c2fae11b3c71d0668c5a34e66a81dc128
ygopk01487 <misublack24@gmail.com>
2024-05-11 08:59:28 +0700

    updates socket io server

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://663ece951107b275f0a0bcb7--storebooks-client.netlify.app",
   },
 });
 
