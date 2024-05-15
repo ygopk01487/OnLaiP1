eec1dd54f58e7169747ce6d91a5a50e2a0be9422
ygopk01487 <misublack24@gmail.com>
2024-05-15 13:09:11 +0700

    update url socker io server

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://storebooks-client.netlify.app",
+    origin: "https://bansach.netlify.app",
   },
 });
 
